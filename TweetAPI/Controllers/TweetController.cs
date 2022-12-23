a76b83343796e2ae6fd437a77f19a981b29800d2
ATRuppelt <alexanderthomasruppelt@gmail.com>
2022-12-23 14:04:56 -0600

    Add project files.

---
+++ TweetAPI/Controllers/TweetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TweetAPI.Models;
+
+namespace TweetAPI.Controllers
+{
+    public class TweetController : Controller
+    {
+        public static TweetModel[] GetTweets()
+        {
+            return new TweetModel[] { };
+        }
+    }
+}
---
+++ TweetUI/ClientApp/src/Tweet.js
@@ -0,0 +1,10 @@
+﻿
+function Tweet(props) {
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+        </div>
+    );
+}
+
+export default Tweet;
